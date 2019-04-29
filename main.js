1ac64881c2db807bea4c203355d0bb2e3aaf185c
clintdowning <49659100+clintdowning@users.noreply.github.com>
2019-04-29 14:12:27 -0400

    Added animation script for task addition.

--- main.js
+++ main.js
@@ -10,7 +10,13 @@ $("#submit").click(function(){  // Add entry on submit.
 });
 
 function add(name, date, assigned ){  // General add.
-	$("table").prepend("<tr><td>" + name + "</td><td>" + date + "</td><td>" + assigned + "</td></tr>");
+	var pos = direction == 'left' ? '-' : '' ;
+	pos += '1000px';
+	direction = direction == 'left' ? 'right' : 'left' ;
+	var index_id = "index_" + index;
+	$("table").prepend("<tr id=\"" + index_id + "\" style=\"left: " + pos + "\"><td>" + name + "</td><td>" + date + "</td><td>" + assigned + "</td></tr>");
+	$("#"+index_id).animate({height: '38px'},250);
+	$("#"+index_id).animate({left: '0px'},500);
 }
 
 function formatDate ( date ) {  // Format date from date input field.
@@ -20,3 +26,6 @@ function formatDate ( date ) {  // Format date from date input field.
 	var fixed_date = month + "/" + day + "/" + year;
 	return fixed_date;
 }
+
+var direction = 'left';
+var index = 0;
